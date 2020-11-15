dd1d6008737e0f38c96c68f0ebe1f099e795d554
Mitchell Schooler <mitchell@schooler.me>
2020-11-15 10:42:14 -0800

    Fetches API data and can assign to variables

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import DriveUtils from './DriveUtils';
+
+const { fetchApi } = DriveUtils();
 
 const App = () => {
-    return <div />
-}
-export default App
+  const [feed, setFeed] = useState();
+
+  useEffect(() => {
+    fetchApi().then((data) => setFeed(data));
+  }, []);
+
+  if (feed) {
+    // dont run until feed state is populated
+    const { iconLink, thumbnailLink, title } = feed.items[0];
+    return (
+      <div>
+        <p>{title}</p>
+        <img src={iconLink} />
+      </div>
+    );
+  }
+
+  return <div>Loading</div>;
+};
+
+export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,6 +7,12 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const sampleResults = require('./sampleResults.json');
+
+app.get('/', function (req, res) {
+  res.send(sampleResults);
+});
 
 app.listen(port);
 console.log('Listening on port', port);
